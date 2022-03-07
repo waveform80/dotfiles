503d904fad999bbed161d77d9c243de017e1fa37
Dave Jones <dave@waveform.org.uk>
2022-03-07 23:00:26 +0000

    Handle different sizes and orientations better

--- compose.bash
+++ compose.bash
@@ -2,12 +2,29 @@
 
 set -eu
 
-view_pane=$(tmux split-window -b -d -h -l 50% -P -F "#D" \
-    neomutt -R -F "$HOME"/dotfiles/neomuttrc."$NEOMUTT_PROFILE")
+width=$(tput cols)
+height=$(tput lines)
+editor_width=84
+editor_height=24
 
-cleanup () {
-    tmux kill-pane -t "${view_pane}" >/dev/null 2>&1
-}
+if [ $width -ge $((editor_width * 2)) ]; then
+    orientation="-h"
+    size=$((width - editor_width))
+elif [ $height -ge $((editor_height * 2)) ]; then
+    orientation="-v"
+    size=$((height - editor_height))
+else
+    orientation=""
+    size=""
+fi
 
-trap cleanup EXIT ERR
+if [ -n "$orientation" ]; then
+    view_pane=$(tmux split-window -b -d $orientation -l $size -P -F "#D" \
+        neomutt -R -F "$HOME"/dotfiles/neomuttrc."$NEOMUTT_PROFILE")
+
+    cleanup () { tmux kill-pane -t "${view_pane}" >/dev/null 2>&1; }
+
+    trap cleanup EXIT ERR
+fi
+
 vim "$@"
